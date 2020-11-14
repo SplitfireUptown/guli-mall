4883b3e6130f7b8beff4c441e159800eff093409
lixiaofei <963842069@qq.com>
2020-11-14 13:58:27 +0800

    update

---
+++ guli-order/src/main/java/com/uptown/guli/guliorder/GuliOrderApplication.java
@@ -0,0 +1,13 @@
+package com.uptown.guli.guliorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliOrderApplication.class, args);
+    }
+
+}
---
+++ guli-ware/src/test/java/com/uptown/guli/guliware/GuliWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptown.guli.guliware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
