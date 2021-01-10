9173ee747f0935d9292c04abb1d1c575d019e141
lixiaofei <963842069@qq.com>
2021-01-10 21:09:34 +0800

    配置网关跨域

---
+++ guli-gateway/src/main/java/com/uptown/guli/gateway/config/GuliCorsConfiguration.java
@@ -0,0 +1,31 @@
+package com.uptown.guli.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @author: Lixiaofei
+ * @date: 2021/1/10 20:56
+ */
+@Configuration
+public class GuliCorsConfiguration {
+
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+
+}
