2091ce89a171cf4eb6a6b545584f73bedd7564e2
lixiaofei <963842069@qq.com>
2021-01-31 22:24:33 +0800

    oss文件上传

--- guli-third-party/src/test/java/com/uptown/guli/GuliThirdPartyApplicationTests.java
+++ guli-third-party/src/test/java/com/uptown/guli/GuliThirdPartyApplicationTests.java
@@ -25,16 +25,11 @@ class GuliThirdPartyApplicationTests {
     @SneakyThrows
     @Test
     public void testUpload() {
-//        String endpoint = "http://oss-cn-beijing.aliyuncs.com";
-//        String accessKeyId = "LTAI4G9niwzj7HGqPruhYFwX";
-//        String accessKeySecret = "fMJj2CQULqH1fAderxYhiWTJQGoNqt";
-//
-//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 
         InputStream inputStream = new FileInputStream("E:\\ChromeDownload\\cbf46ef427ded17a21be6b9190e7c678.jpeg");
         ossClient.putObject("guli-uptown", "me.jpg", inputStream);
 
-// 关闭OSSClient。
+        // 关闭OSSClient。
         ossClient.shutdown();
 
         System.out.println("success");
