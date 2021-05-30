2a8ae9f00edae947985c9fabbb5b1cbcee39df14
lixiaofei <963842069@qq.com>
2021-05-30 19:17:58 +0800

    update

--- guli-search/src/main/java/com/uptown/guli/search/config/GuliElasticsearchConfig.java
+++ guli-search/src/main/java/com/uptown/guli/search/config/GuliElasticsearchConfig.java
@@ -1,6 +1,7 @@
 package com.uptown.guli.search.config;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GuliElasticsearchConfig {
 
+
+    public static final RequestOptions COMMON_OPTIONS;
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+        //builder.addHeader("Authorization", "Bearer " + TOKEN);
+//        builder.setHttpAsyncResponseConsumerFactory(
+//                new HttpAsyncResponseConsumerFactory
+//                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        COMMON_OPTIONS = builder.build();
+    }
 
     public RestHighLevelClient EsRestClient() {
         return new RestHighLevelClient(
