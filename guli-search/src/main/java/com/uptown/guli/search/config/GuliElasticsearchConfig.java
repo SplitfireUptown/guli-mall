186a8de1e6be28292ec35276b28290126d4c741b
lixiaofei <963842069@qq.com>
2021-05-30 19:20:57 +0800

    update

--- guli-search/src/main/java/com/uptown/guli/search/config/GuliElasticsearchConfig.java
+++ guli-search/src/main/java/com/uptown/guli/search/config/GuliElasticsearchConfig.java
@@ -17,6 +17,7 @@ public class GuliElasticsearchConfig {
     public static final RequestOptions COMMON_OPTIONS;
     static {
         RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+
         //builder.addHeader("Authorization", "Bearer " + TOKEN);
 //        builder.setHttpAsyncResponseConsumerFactory(
 //                new HttpAsyncResponseConsumerFactory
